1db80e3c6e9239a6274e4ccdc83784abaa7b9279
kuyesu <2019bce028@std.must.ac.ug>
2022-11-20 12:28:33 +0300

    get products props

---
+++ component/productsCard.tsx
@@ -0,0 +1,46 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+import { formatAsCurrency } from "./../lib/util";
+interface ProductCardProps {
+  product: Product;
+  usePurchaseButton?: boolean;
+}
+const ProductCard = (props: ProductCardProps) => {
+  const { product } = props;
+
+  return (
+    <div className="border-gray-300 border rounded-lg py-8 px-6">
+      <div className="text-center">
+        <Image
+          src={product.image}
+          width={200}
+          height={200}
+          alt={"product Image"}
+        />
+      </div>
+      <p className="font-bold text-2xl text-gray-700 mt-4">{product.name}</p>
+      <p className="text-gray-500 my-4">{product.description}</p>
+      <p className="font-bold text-2xl mb-4">
+        {formatAsCurrency(props.product.price)}
+      </p>
+      {props.usePurchaseButton ? (
+        <Link
+          href={`/products/${product.id}`}
+          className="bg-purple-700 text-white rounded-full py-2 px-4 text-sm hover:bg-purple-800 shadow-md"
+        >
+          Buy Now
+        </Link>
+      ) : (
+        <Link
+          href={`/products/${product.id}`}
+          className="bg-purple-700 text-white rounded-full py-2 px-4 text-sm hover:bg-purple-800 shadow-md"
+        >
+          View Details
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default ProductCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
+import {Product} from '@prisma/client'
 import Head from 'next/head'
 import Image from 'next/image'
+import ProductCard from '../component/productsCard'
 import styles from '../styles/Home.module.css'
+import prisma from './../db'
 
-export default function Home() {
+export async function getServerSideProps() {
+  try {
+    const products = await prisma.product.findMany();
+    return {
+      props: { products },
+    };
+  } catch (err) {
+    return {
+      props: { products: [] },
+    };
+  }
+}
+
+interface HomeProps {
+  products: Product[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,40 +37,16 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {/* <a href="https://nextjs.org/learn" className={styles.card}> */}
+            {props.products.map((p, i: number) => (
+              <ProductCard product={p} key={i} />
+            ))}
+          {/* </a> */}
         </div>
       </main>
 
@@ -60,12 +56,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
